1738d52295318bd60303838680b18e2f70b3c88b
Lucas Gonzalez <lucas.gonzalez.espinosa@outlook.com>
2024-02-21 14:41:21 -0300

    Refactor component exports and update dashboard layout

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { routes } from '../../app.routes';
 
 @Component({
   selector: 'app-sidemenu',
@@ -9,4 +10,19 @@ import { Component } from '@angular/core';
 })
 export class SidemenuComponent {
 
+  public menuItems = routes // Rutas principales
+  .map( route => route.children ?? [])  // Para obtener solo las rutas hijas dentro de la ruta principal
+  .flat() // Para aplanar el arreglo de arreglos
+  .filter( route => route && route.path) // Para obtener solo las rutas que tengan un path
+  .filter( route => !route.path?.includes(':'))
+
+  constructor() {
+    // const dashboardRoute = routes // Rutas principales
+    // .map( route => route.children ?? [])  // Para obtener solo las rutas hijas dentro de la ruta principal
+    // .flat() // Para aplanar el arreglo de arreglos
+    // .filter( route => route && route.path) // Para obtener solo las rutas que tengan un path
+    // .filter( route => !route.path?.includes(':'))
+
+    // console.log(dashboardRoute)
+  }
 }
