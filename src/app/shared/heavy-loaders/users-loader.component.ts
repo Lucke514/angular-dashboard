cfbe1814e7dfef8a9009837ecba2160edfb8a878
Lucas Gonzalez <lucas.gonzalez.espinosa@outlook.com>
2024-02-21 12:42:05 -0300

    first commit

---
+++ src/app/shared/heavy-loaders/users-loader.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-loader',
+  standalone: true,
+  imports: [],
+  template: `
+    <h1>Users</h1>
+  `
+})
+
+export class UserLoaderComponet {
+  constructor() { }
+}
